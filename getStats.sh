11d819cc663e77ca068f459c37ee5d3f7b67610e
Sergey Koren <sergek@umd.edu>
2019-07-12 09:48:40 -0400

    Add checks for prereq and parameters, add a definition of "attempted" BAC to count assembly errors

--- getStats.sh
+++ getStats.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# sanity check inputs/prereq
+if [ $# -lt 1 ]; then
+   echo "Error: I need an assembly as the first parameter"
+   exit
+fi
+
+haveBed=`which bedtools 2>/dev/null |wc -l`
+if [ $haveBed -eq 0 ]; then
+   echo "Error: no bedtools found in path, please add bedtools to your path before running"
+   exit
+fi
+
+haveSam=`which samtools 2>/dev/null |wc -l`
+if [ $haveSam -eq 0 ]; then
+   echo "Error: no samtools found in path, please add samtools to your path before running"
+   exit
+fi
+
+haveMinimap=`which minimap2 2>/dev/null |wc -l`
+if [ $haveMinimap -eq 0 ]; then
+   echo "Error: no minimap2 found in path, please add minimap2 to your path before running"
+   exit
+fi
+
 ASM=$1
 BREAK_LEN=2000
 if [ $# -ge 2 ]; then
@@ -44,6 +68,12 @@ fi
 samtools faidx bacs.fasta
 total=`grep -c ">" bacs.fasta`
 bp=`cat bacs.fasta.fai |awk '{SUM+=$2; } END {print SUM}'`
+# figure out attempted, we have a bit more tolerance than the 995 for resolved here, just 95 to account for indels in the attempted reconstruction
+# first we convert the paf to a BED with tig name and BAC length included
+# the groupby will give a list of mapping positions on each contig, like this:
+# AC275637.1 tig00000007     0,151967,152735,57006   49467,153339,173266,139925      173266
+# then we just go through and sum up the intervals and divide
+cat $i |awk '{print $1"\t"$6"\t"$7"\t"$7-$6"\t"$2"\t"$8}'|sort -gk1 |sort -k1,1 -k 5,5 |bedtools groupby -g 1,5 -c 2,3,6 -o collapse,collapse,max -i - |awk -F "\t" '{s=split($3, S, ","); e=split($4, E, ","); if (e != s) { print "Error: non-matching intervals"; } else { SUM=0; for (i = 1; i <= s; i++) { SUM+=E[i]-S[i]; } if (SUM/$NF > 0.95) print $1}}'|sort |uniq > $PREFIX.attempted
 
 echo "******************* BAC SUMMARY ******************"
 echo " TOTAL    : $total"
@@ -51,9 +81,11 @@ echo " BP       : $bp"
 
 echo "************** Statistics for: $ASM ****************"
 cat $PREFIX.txt |awk '{if (($7-$6)/$8 > 0.995) print $1}'|sort |uniq |awk '{print $1}' > $PREFIX.resolved 
+ATTEMPTED=`cat $PREFIX.attempted |wc -l`
+FAILED=`cat $PREFIX.attempted |grep -v -f $PREFIX.resolved |wc -l`
 DONE=`cat $PREFIX.txt |awk '{if (($7-$6)/$8 > 0.995) print $1}'|sort |uniq |awk '{print $1}' |wc -l`
 DONEBP=`cat $PREFIX.txt |awk '{if (($7-$6)/$8 > 0.995) print $1" "$8}'|sort |uniq |awk '{SUM+=$NF; } END { print SUM}'`
-echo "$DONE $total $DONEBP $bp" |awk '{print "BACs closed: "$1" ("$1/$2*100") BASES "$3" ("$3/$4*100")"}'
+echo "$DONE $total $DONEBP $bp $FAILED" |awk '{print "BACs closed: "$1" ("$1/$2*100"); BACs attempted: "$1+$NF" %good = "($1/($1+$NF))*100"; BASES "$3" ("$3/$4*100")"}'
    
 echo "" > Rstats
 echo "dat = read.table('$PREFIX.R')" >> Rstats
